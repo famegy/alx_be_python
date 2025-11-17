224c172e55a845a8055a131ab50a386d1cd01ebf
SAMUEL GIKONYO <samuelgiks254@gmail.com>
2025-11-17 13:17:18 +0300

    Correct cold weather advice print statement
    Fixed formatting issue in cold weather advice.

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -4,10 +4,7 @@ if weather == "sunny":
 elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
 elif weather == "cold":
-<<<<<<< HEAD
     print( Make sure to wear a warm coat and a scarf.")
-=======
     print("Make sure to wear a warm coat and  scarf.")
->>>>>>> d8757bc (adding match calculator code)
 else:
     print("Sorry, I don't have recommendations for this weather.")
