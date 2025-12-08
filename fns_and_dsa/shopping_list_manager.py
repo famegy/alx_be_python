b2b7afd4f9c76033c0923644304d1fec82968771
SAMUEL GIKONYO <samuelgiks254@gmail.com>
2025-12-08 06:37:52 +0300

    Fix input handling for shopping list choices

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -8,25 +8,34 @@ def main():
     shopping_list = []
     while True:
         display_menu()
-        choice = input("Enter your choice: ")
+        choice = input("Enter your choice (1-4): ")
+            try:
+                choice_num = int(choice)
+                if 1 <= choice_num <= 4:
+                    break  # valid number
+                else:
+                    print("Please enter a number between 1 and 4.")
+            except ValueError:
+                print("Invalid input. Please enter a number.")
 
-        if choice == "1":
+
+        if choice_num == "1":
             item = input("Enter the item to be added: ")
             shopping_list.append(item)
             print(f"{item} has been added")
-        elif choice == "2":
+        elif choice_num == "2":
             item = input("Enter item to be removed: ")
             if item in shopping_list:
                 shopping_list.remove(item)
                 print(f"{item} has been removed")
             else:
                 print(f"{item} not found")
-        elif choice == "3":
+        elif choice_num == "3":
             if shopping_list:
                 print("shopping list items:")
                 for item in shopping_list:
                     print(f"- {item}")
-        elif choice == "4":
+        elif choice_num == "4":
             print("Exiting the Shopping List Manager. Goodbye!")
             break
         else:
