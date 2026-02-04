11b317314b381484320e756eeb3654af9814da18
SAMUEL GIKONYO <samuelgiks254@gmail.com>
2026-02-04 15:11:04 +0300

    Replace get_details with __str__ method in Book classes

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,7 +3,7 @@ class Book:
         self.title = title
         self.author = author
 
-    def get_details(self) -> str:
+    def __str__(self) -> str:
         return f"Book: {self.title} by {self.author}"
 
 
@@ -12,7 +12,7 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size = file_size
 
-    def get_details(self) -> str:
+    def __str__(self) -> str:
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
@@ -21,7 +21,7 @@ class PrintBook(Book):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def get_details(self) -> str:
+    def __str__(self) -> str:
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
